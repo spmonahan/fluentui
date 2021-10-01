437811e5ece217d885606d5c78a40a66ebff04f3
Lingfan Gao <lingfangao@hotmail.com>
2021-10-01 11:35:37 +0200

    update beachball to publish to private registry

--- scripts/beachball/index.ts
+++ scripts/beachball/index.ts
@@ -1,5 +1,6 @@
 import { BeachballConfig } from 'beachball';
-import { renderHeader, renderEntry } from './customRenderers';
+import { writeFile } from 'fs-extra';
+import * as path from 'path';
 import { getScopes } from './getScopes';
 import { getVNextChangelogGroups } from './getVNextChangelogGroups';
 
@@ -7,13 +8,29 @@ export const config: BeachballConfig = {
   disallowedChangeTypes: ['major', 'prerelease'],
   tag: 'latest',
   generateChangelog: true,
-  groupChanges: true,
+  gitTags: false,
   scope: getScopes(),
+  registry:
+    'https://uifabric.pkgs.visualstudio.com/4ed167b9-ac3a-405b-b967-443af8db8961/_packaging/ling-test/npm/registry/',
   changelog: {
-    customRenderers: {
-      renderHeader,
-      renderEntry,
-    },
     groups: [getVNextChangelogGroups()],
+  },
+  hooks: {
+    prepublish: packagePath => {
+      const authToken = process.env.ADO_TOKEN;
+      return writeFile(
+        path.join(packagePath, '.npmrc'),
+        `
+; begin auth token
+//uifabric.pkgs.visualstudio.com/4ed167b9-ac3a-405b-b967-443af8db8961/_packaging/ling-test/npm/registry/:username=uifabric
+//uifabric.pkgs.visualstudio.com/4ed167b9-ac3a-405b-b967-443af8db8961/_packaging/ling-test/npm/registry/:_password=${authToken}
+//uifabric.pkgs.visualstudio.com/4ed167b9-ac3a-405b-b967-443af8db8961/_packaging/ling-test/npm/registry/:email=npm requires email to be set but doesn't use the value
+//uifabric.pkgs.visualstudio.com/4ed167b9-ac3a-405b-b967-443af8db8961/_packaging/ling-test/npm/:username=uifabric
+//uifabric.pkgs.visualstudio.com/4ed167b9-ac3a-405b-b967-443af8db8961/_packaging/ling-test/npm/:_password=${authToken}
+//uifabric.pkgs.visualstudio.com/4ed167b9-ac3a-405b-b967-443af8db8961/_packaging/ling-test/npm/:email=npm requires email to be set but doesn't use the value
+; end auth token
+      `,
+      );
+    },
   },
 };
