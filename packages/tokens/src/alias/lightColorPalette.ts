afd994736fc3fe3a088c38a100836ff832f0a4ea
Sean Monahan <seanmonahan@microsoft.com>
2024-07-16 16:51:44 -0700

    remove supporting palette color code

--- packages/tokens/src/alias/lightColorPalette.ts
+++ packages/tokens/src/alias/lightColorPalette.ts
@@ -1,44 +1,46 @@
-import { statusSharedColors, personaSharedColors, mappedStatusColors } from '../global/colorPalette';
-import { statusSharedColorNames, personaSharedColorNames } from '../sharedColorNames';
-import { ColorPaletteTokens, ColorStatusTokens, PersonaColorPaletteTokens, StatusColorPaletteTokens } from '../types';
+import { /*statusSharedColors, personaSharedColors,*/ mappedStatusColors } from '../global/colorPalette';
+// import { statusSharedColorNames, personaSharedColorNames } from '../sharedColorNames';
+import {
+  /*ColorPaletteTokens,*/ ColorStatusTokens /*PersonaColorPaletteTokens, StatusColorPaletteTokens*/,
+} from '../types';
 import { statusColorMapping } from '../statusColorMapping';
 
-const statusColorPaletteTokens = statusSharedColorNames.reduce((acc, sharedColor) => {
-  const color = sharedColor.slice(0, 1).toUpperCase() + sharedColor.slice(1);
-  const sharedColorTokens = {
-    [`colorPalette${color}Background1`]: statusSharedColors[sharedColor].tint60,
-    [`colorPalette${color}Background2`]: statusSharedColors[sharedColor].tint40,
-    [`colorPalette${color}Background3`]: statusSharedColors[sharedColor].primary,
-    [`colorPalette${color}Foreground1`]: statusSharedColors[sharedColor].shade10,
-    [`colorPalette${color}Foreground2`]: statusSharedColors[sharedColor].shade30,
-    [`colorPalette${color}Foreground3`]: statusSharedColors[sharedColor].primary,
-    [`colorPalette${color}BorderActive`]: statusSharedColors[sharedColor].primary,
-    [`colorPalette${color}Border1`]: statusSharedColors[sharedColor].tint40,
-    [`colorPalette${color}Border2`]: statusSharedColors[sharedColor].primary,
-  };
+// const statusColorPaletteTokens = statusSharedColorNames.reduce((acc, sharedColor) => {
+//   const color = sharedColor.slice(0, 1).toUpperCase() + sharedColor.slice(1);
+//   const sharedColorTokens = {
+//     [`colorPalette${color}Background1`]: statusSharedColors[sharedColor].tint60,
+//     [`colorPalette${color}Background2`]: statusSharedColors[sharedColor].tint40,
+//     [`colorPalette${color}Background3`]: statusSharedColors[sharedColor].primary,
+//     [`colorPalette${color}Foreground1`]: statusSharedColors[sharedColor].shade10,
+//     [`colorPalette${color}Foreground2`]: statusSharedColors[sharedColor].shade30,
+//     [`colorPalette${color}Foreground3`]: statusSharedColors[sharedColor].primary,
+//     [`colorPalette${color}BorderActive`]: statusSharedColors[sharedColor].primary,
+//     [`colorPalette${color}Border1`]: statusSharedColors[sharedColor].tint40,
+//     [`colorPalette${color}Border2`]: statusSharedColors[sharedColor].primary,
+//   };
 
-  return Object.assign(acc, sharedColorTokens);
-}, {} as StatusColorPaletteTokens);
+//   return Object.assign(acc, sharedColorTokens);
+// }, {} as StatusColorPaletteTokens);
 
-// one-off patch for yellow
-statusColorPaletteTokens.colorPaletteYellowForeground1 = statusSharedColors.yellow.shade30;
+// // one-off patch for yellow
+// statusColorPaletteTokens.colorPaletteYellowForeground1 = statusSharedColors.yellow.shade30;
 
-statusColorPaletteTokens.colorPaletteRedForegroundInverted = statusSharedColors.red.tint20;
-statusColorPaletteTokens.colorPaletteGreenForegroundInverted = statusSharedColors.green.tint20;
-statusColorPaletteTokens.colorPaletteYellowForegroundInverted = statusSharedColors.yellow.tint40;
+// statusColorPaletteTokens.colorPaletteRedForegroundInverted = statusSharedColors.red.tint20;
+// statusColorPaletteTokens.colorPaletteGreenForegroundInverted = statusSharedColors.green.tint20;
+// statusColorPaletteTokens.colorPaletteYellowForegroundInverted = statusSharedColors.yellow.tint40;
 
-const personaColorPaletteTokens = personaSharedColorNames.reduce((acc, sharedColor) => {
-  const color = sharedColor.slice(0, 1).toUpperCase() + sharedColor.slice(1);
-  const sharedColorTokens = {
-    [`colorPalette${color}Background2`]: personaSharedColors[sharedColor].tint40,
-    [`colorPalette${color}Foreground2`]: personaSharedColors[sharedColor].shade30,
-    [`colorPalette${color}BorderActive`]: personaSharedColors[sharedColor].primary,
-  };
+// const personaColorPaletteTokens = personaSharedColorNames.reduce((acc, sharedColor) => {
+//   const color = sharedColor.slice(0, 1).toUpperCase() + sharedColor.slice(1);
+//   const sharedColorTokens = {
+//     [`colorPalette${color}Background2`]: personaSharedColors[sharedColor].tint40,
+//     [`colorPalette${color}Foreground2`]: personaSharedColors[sharedColor].shade30,
+//     [`colorPalette${color}BorderActive`]: personaSharedColors[sharedColor].primary,
+//   };
 
-  return Object.assign(acc, sharedColorTokens);
-}, {} as PersonaColorPaletteTokens);
+//   return Object.assign(acc, sharedColorTokens);
+// }, {} as PersonaColorPaletteTokens);
 
-export const colorPaletteTokens: ColorPaletteTokens = { ...statusColorPaletteTokens, ...personaColorPaletteTokens };
+// export const colorPaletteTokens: ColorPaletteTokens = { ...statusColorPaletteTokens, ...personaColorPaletteTokens };
 
 export const colorStatusTokens: ColorStatusTokens = Object.entries(statusColorMapping).reduce(
   (acc, [statusColor, sharedColor]) => {
